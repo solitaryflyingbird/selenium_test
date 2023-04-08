a7458a77cf518671412d009cd2d4d0cf08f3f5fe
solitaryflyingbird <fkdlcb777@gmail.com>
2023-04-08 19:32:40 +0900

    iframe 대충 뚫음

--- class 보기.py
+++ class 보기.py
@@ -9,21 +9,9 @@ import time
 driver = webdriver.Chrome(executable_path='path/to/chromedriver')
 attack = 'https://gall.dcinside.com/mgallery/board/write/?id=whitehouse'
 driver.get(attack)
-iframe = driver.find_element("name", "tx_canvas_wysiwyg")
+iframe = driver.find_element(By.CSS_SELECTOR,'iframe')
 driver.switch_to.frame(iframe)
-elements = driver.find_elements(By.XPATH, "//*")
-sucess = []
-for element in elements:
 
-    try:
-        element.send_keys("ddd")
-        sucess +=[element]
-        print(element,"sucess")
-    except:
-        print(element, "false")
-        pass
 ttt = driver.find_element(By.CLASS_NAME, "tx-content-container")
-ttt.send_keys("x")
-print("end")
-print(sucess)
-time.sleep(10)
+
+ttt.send_keys("ddd")
