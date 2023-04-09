b879ed3d584fd16ccf507d7ae4a8d14fbc9396e6
solitaryflyingbird <fkdlcb777@gmail.com>
2023-04-09 18:38:01 +0900

    문제는 헤더인듯

--- 헤더변경.py
+++ 헤더변경.py
@@ -1,18 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 import time
 
-# Replace 'path/to/chromedriver' with the actual path to your chromedriver executable
-driver = webdriver.Chrome(executable_path='path/to/chromedriver')
+options = Options()
+options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
+
+capabilities = DesiredCapabilities.CHROME.copy()
+capabilities['goog:chromeOptions'] = options.to_capabilities()
+driver = webdriver.Chrome(executable_path='path/to/chromedriver', desired_capabilities=capabilities)
 
 
 attack = "https://gall.dcinside.com/board/write/?id=football_new8"
 
-# Replace 'https://example.com' with the website containing the button
+# 셀레니움 와이어로 헤더 변경
+def interceptor(request):
+    del request.headers['Referer']
+    del request.headers['User-Agent']
+    request.headers['Referer'] = 'https://gall.dcinside.com/board/write/?id=football_new8'
+    request.headers['User-Agent'] = "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+driver.request_interceptor = interceptor
+
 driver.get(attack)
 
 
@@ -27,11 +38,7 @@ def test(arr):
 
 
 try:
-    dccon = driver.find_element(By.XPATH, "//a[@class='tx-text tx_dccon']")
-    dccon.click()
-    time.sleep(2)
-    dccon.click()
-    time.sleep(2)
+
     time.sleep(1)
     nickname_input = driver.find_element("name", "name")
     password_input = driver.find_element("name", "password")
@@ -44,13 +51,14 @@ try:
     iframe = driver.find_element("name", "tx_canvas_wysiwyg")
     driver.switch_to.frame(iframe)
     main_input = driver.find_element(By.CLASS_NAME, "tx-content-container")
-    main_input.send_keys("소농민 vs 박지숭 vs 좆두")
+    main_input.send_keys("x")
     driver.switch_to.default_content()
 
- 
+
     button = driver.find_element(By.CSS_SELECTOR, '.btn_blue.btn_svc.write')
     button.click()
-    time.sleep(17)
+    print("click")
+    time.sleep(1227)
 
 
 
